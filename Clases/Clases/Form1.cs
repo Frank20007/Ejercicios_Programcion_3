20fe60e83edce391900c3f08780b169d34a3f8ed
Frank20007 <86393402+Frank20007@users.noreply.github.com>
2021-06-29 20:04:17 -0600

    Clases

---
+++ Clases/Clases/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Clases
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnEjecutar_Click(object sender, EventArgs e)
+        {
+            //Persona Pedro = new Persona();
+
+            //Pedro.Nombre = NombreTextBox.Text;
+            //Pedro.Apellido = ApellidoTextBox.Text;
+            //Pedro.Edad = Convert.ToInt32(EdadTextBox.Text);
+            //Pedro.Direccion = DireccionTextBox.Text;
+
+            Personas Pedro = new Personas(txtNombre .Text, txtApellido .Text, Convert.ToInt32(txtEdad .Text), txtDireccion .Text);
+
+            string NombreCompleto = Pedro.DevolverNombreCompleto();
+
+            MessageBox.Show(NombreCompleto);
+
+
+
+        }
+    }
+}
---
+++ Clases/Clases/Personas.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clases
+{
+  public class Personas
+    {
+        //Atributos
+        private string _nombre;
+        private string _apellido;
+        private int _edad;
+        private string _direccion;
+
+        //Propiedades
+        //public string Nombre
+        //{
+        //    get { return _nombre; }
+        //    set { _nombre = value; }
+        //}
+        //public string Apellido
+        //{
+        //    get { return _apellido; }
+        //    set { _apellido = value; }
+        //}
+        //public int Edad
+        //{
+        //    get { return _edad; }
+        //    set { _edad = value; }
+        //}
+        //public string Direccion
+        //{
+        //    get { return _direccion; }
+        //    set { _direccion = value; }
+        //}
+
+        //Otro tipo de propiedades
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public int Edad { get; set; }
+        public string Direccion { get; set; }
+
+        //Constructores
+        public Personas()
+        {
+        }
+        public Personas(string nombre, string apellido, int edad, string direccion)
+        {
+            Nombre = nombre;
+            Apellido = apellido;
+            Edad = edad;
+            Direccion = direccion;
+        }
+
+        public Personas(string nombre, string apellido)
+        {
+            Nombre = nombre;
+            Apellido = apellido;
+        }
+
+        //Métodos o Funciones (Acciones)
+        public string DevolverNombreCompleto()
+        {
+            return Nombre + " " + Apellido;
+        }
+
+    }
+}
