bd251a335d49621124a8f312aef4cfb4752dab3f
Frank20007 <86393402+Frank20007@users.noreply.github.com>
2021-07-02 21:25:51 -0600

    Calificaciones

---
+++ Calificaciones/Calificaciones/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calificaciones
+{
+    public partial class frmCalificaciones : Form
+    {
+        public frmCalificaciones()
+        {
+            InitializeComponent();
+        }
+
+        private void btnEjecutar_Click(object sender, EventArgs e)
+        {
+            Promedio Estudiante = new Promedio(txtNombre.Text,Convert .ToInt32 ( txtCuenta.Text),Convert .ToInt32 ( txtNota1.Text),Convert .ToInt32 ( txtNota2.Text),Convert .ToInt32( txtNota3.Text),Convert .ToInt32 ( txtNota4.Text));
+
+            string promedio = Estudiante.DevolverPromnedio();
+
+            MessageBox.Show(promedio);
+
+
+            string aprobadoreprobado = Estudiante.AprobadoReprobado();
+
+            MessageBox.Show(aprobadoreprobado);
+
+        }
+    }
+}
